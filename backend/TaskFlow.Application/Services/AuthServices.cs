164ccc8d8b7e54a1385b41bf6ddc388fb6f9a4a4
Vanessa Oliveira <vanessa.oliveira.80@gmail.com>
2023-10-08 23:10:34 -0300

    feat: serviço responsável pelo hash de senhas

---
+++ backend/TaskFlow.Application/Services/AuthServices.cs
@@ -0,0 +1,27 @@
+﻿using TaskFlow.Infrastructure.Contracts;
+
+namespace TaskFlow.Application.Services;
+
+public class AuthService : IAuthService
+{
+    private readonly IUserRepository _userRepository;
+
+    public AuthService(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    public string HashPassword(string password)
+    {
+        string salt = BCrypt.Net.BCrypt.GenerateSalt(12);
+        string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password, salt);
+        return hashedPassword;
+    }
+
+    public async Task<bool> VerifyPassword(string userEmail, string password)
+    {
+        var user = await _userRepository.FindUserByEmail(userEmail);
+        bool passwordMatch = BCrypt.Net.BCrypt.Verify(password, user.PasswordHash);
+        return passwordMatch;
+    }
+}
---
+++ backend/TaskFlow.Infrastructure/Contracts/IAuthService.cs
@@ -0,0 +1,7 @@
+﻿namespace TaskFlow.Infrastructure.Contracts;
+
+public interface IAuthService
+{
+    public string HashPassword(string password);
+    public Task<bool> VerifyPassword(string userEmail, string password);
+}
